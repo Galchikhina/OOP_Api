b03a4456e9c17fa4ea6a01b77b70bf8961b0d2fc
ElenaG <lenok1586@mail.ru>
2022-07-19 21:48:52 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String passportSeriesAndNumber;
+    public String phone;
+    public String patronymic;
+    public String surname;
+       public boolean subscription;
+
+       FormDate birthday = new FormDate();
+}
